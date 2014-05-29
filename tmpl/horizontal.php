0e5618ac57d657be1e61157a88a9cf0910efc1b6
root <sosojni@hotmail.com>
2014-05-29 16:23:59 +0200

    class name fix-ing
    syncing class names with module name

--- tmpl/horizontal.php
+++ tmpl/horizontal.php
@@ -9,29 +9,29 @@
 
 defined('_JEXEC') or die;
 ?>
-<div class="rapid_contact horizontal <?php echo $mod_class_suffix; ?>">
+<div class="razor_contact horizontal <?php echo $mod_class_suffix; ?>">
 	<form action="<?php echo $url; ?>" method="POST" enctype="multipart/form-data">
-		<div class="rapid_contact intro_text <?php echo $mod_class_suffix; ?>">
+		<div class="razor_contact intro_text <?php echo $mod_class_suffix; ?>">
 			<?php echo $pre_text; ?>
 		</div>
 		<?php if ($myError != '') : ?>
 		  <?php echo $myError; ?>
 		<?php endif; ?>
 		<?php if ($enable_anti_spam && $anti_spam_position == 0) : ?>
-			<?php echo $myAntiSpamQuestion; ?><input class="rapid_contact inputbox <?php echo $mod_class_suffix; ?>" type="text" name="rp_anti_spam_answer" size="<?php echo $emailWidth; ?>" value="<?php echo $CORRECT_ANTISPAM_ANSWER; ?>"/>
+			<?php echo $myAntiSpamQuestion; ?><input class="razor_contact inputbox <?php echo $mod_class_suffix; ?>" type="text" name="rp_anti_spam_answer" size="<?php echo $emailWidth; ?>" value="<?php echo $CORRECT_ANTISPAM_ANSWER; ?>"/>
 		<?php endif; ?>
 		<div class="column1">
 			<div class="subject-lable">
 				<?php echo $mySubjectLabel; ?>
 			</div>
 			<div class="subject-input">
-				<input class="rapid_contact inputbox <?php echo $mod_class_suffix; ?>" type="text" name="rp_subject" size="<?php echo $subjectWidth; ?>" value="<?php echo $CORRECT_SUBJECT; ?>"/>
+				<input class="razor_contact inputbox <?php echo $mod_class_suffix; ?>" type="text" name="rp_subject" size="<?php echo $subjectWidth; ?>" value="<?php echo $CORRECT_SUBJECT; ?>"/>
 			</div>
 			<div class="email-lable">
 				<?php echo $myEmailLabel; ?>
 			</div>
 			<div class="email-input">
-				<input class="rapid_contact inputbox <?php echo $mod_class_suffix; ?>" type="text" name="rp_email" size="<?php echo $emailWidth; ?>" value="<?php echo $CORRECT_EMAIL; ?>"/>
+				<input class="razor_contact inputbox <?php echo $mod_class_suffix; ?>" type="text" name="rp_email" size="<?php echo $emailWidth; ?>" value="<?php echo $CORRECT_EMAIL; ?>"/>
 			</div>
 			<div class="fileUpload">
 			    <span class="upload-image">+ </span><span><?php echo $uploadButtonText; ?></span>
@@ -43,15 +43,15 @@ defined('_JEXEC') or die;
 				<?php echo $myMessageLabel; ?>
 			</div>
 			<div class="message-input">
-				<textarea class="rapid_contact textarea <?php echo $mod_class_suffix; ?>" name="rp_message" cols="<?php echo $messageWidth; ?>" rows="4"><?php echo $CORRECT_MESSAGE; ?></textarea>
+				<textarea class="razor_contact textarea <?php echo $mod_class_suffix; ?>" name="rp_message" cols="<?php echo $messageWidth; ?>" rows="4"><?php echo $CORRECT_MESSAGE; ?></textarea>
 			</div>
 			<?php if ($enable_anti_spam && $anti_spam_position == 1) : ?>
 				<?php echo $myAntiSpamQuestion; ?>
-				<input class="rapid_contact inputbox <?php echo $mod_class_suffix; ?>" type="text" name="rp_anti_spam_answer" size="<?php echo $emailWidth; ?>" value="<?php echo $CORRECT_ANTISPAM_ANSWER; ?>"/>
+				<input class="razor_contact inputbox <?php echo $mod_class_suffix; ?>" type="text" name="rp_anti_spam_answer" size="<?php echo $emailWidth; ?>" value="<?php echo $CORRECT_ANTISPAM_ANSWER; ?>"/>
 			<?php endif; ?>
 			<?php echo JHtml::_( 'form.token' ); ?>
 			<input type="hidden" name="form_id" value="<?php echo $module->id; ?>">
-			<input class="rapid_contact button <?php echo $mod_class_suffix; ?>" type="submit" value="<?php echo $buttonText; ?>" style="width: <?php echo $buttonWidth; ?>%"/>
+			<input class="razor_contact button <?php echo $mod_class_suffix; ?>" type="submit" value="<?php echo $buttonText; ?>" style="width: <?php echo $buttonWidth; ?>%"/>
 		</div>
 	</form>
 </div>
